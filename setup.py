da75d2d5da0db95bad2b5e0729db6cbe5d5871d8
anthonybaxter <>
2001-08-09 13:42:38 +0000

    initial setup.py. That was easy. :)

---
+++ setup.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# 
+# $Id$
+#
+
+from distutils.core import setup
+
+setup ( name = "roundup", 
+	version = "0.?.?",
+	description = "Python DNS library.",
+	author_email = "pydns-developer@lists.sourceforge.net",
+	url = 'http://pydns.sourceforge.net/',
+	packages = ['DNS',]
+	)
+
+#
+# $Log$
+#
