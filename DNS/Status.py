bfcd64502cab2fb380f2c632c04d0bcb872429e3
anthonybaxter <>
2002-04-23 10:57:57 +0000

    update to complete the list of response codes.

--- DNS/Status.py
+++ DNS/Status.py
@@ -9,12 +9,24 @@
  Status values in message header
 """
 
-NOERROR       = 0
-FORMERR       = 1
-SERVFAIL      = 2
-NXDOMAIN      = 3
-NOTIMP        = 4
-REFUSED       = 5
+NOERROR   = 0 #   No Error                           [RFC 1035]
+FORMERR   = 1 #   Format Error                       [RFC 1035]
+SERVFAIL  = 2 #   Server Failure                     [RFC 1035]
+NXDOMAIN  = 3 #   Non-Existent Domain                [RFC 1035]
+NOTIMP    = 4 #   Not Implemented                    [RFC 1035]
+REFUSED   = 5 #   Query Refused                      [RFC 1035]
+YXDOMAIN  = 6 #   Name Exists when it should not     [RFC 2136]
+YXRRSET   = 7 #   RR Set Exists when it should not   [RFC 2136]
+NXRRSET   = 8 #   RR Set that should exist does not  [RFC 2136]
+NOTAUTH   = 9 #   Server Not Authoritative for zone  [RFC 2136]
+NOTZONE   = 10 #  Name not contained in zone         [RFC 2136]
+BADVERS   = 16 #  Bad OPT Version                    [RFC 2671]
+BADSIG 	  = 16 #  TSIG Signature Failure             [RFC 2845]
+BADKEY    = 17 #  Key not recognized                 [RFC 2845]
+BADTIME   = 18 #  Signature out of time window       [RFC 2845]
+BADMODE   = 19 #  Bad TKEY Mode                      [RFC 2930]
+BADNAME   = 20 #  Duplicate key name                 [RFC 2930]
+BADALG 	  = 21 #  Algorithm not supported            [RFC 2930]
 
 # Construct reverse mapping dictionary
 
@@ -29,6 +41,10 @@ def statusstr(status):
 
 #
 # $Log$
+# Revision 1.5  2002/03/19 12:41:33  anthonybaxter
+# tabnannied and reindented everything. 4 space indent, no tabs.
+# yay.
+#
 # Revision 1.4  2002/03/19 12:26:13  anthonybaxter
 # death to leading tabs.
 #
