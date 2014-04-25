737fcb4ee1d2ff0a4dd997571d0132178221a5e3
Scott Kitterman <scott@kitterman.com>
2014-04-25 00:05:14 -0400

    Remove ancient CVS commit info from setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-# 
-# $Id$
-#
-
 import sys,os
 
 sys.path.insert(0,os.getcwd())
@@ -37,30 +33,3 @@ setup(
       ]
 )
 
-#
-# $Log$
-# Revision 1.4.2.3.2.1  2011/02/18 19:35:22  customdesigned
-# Python3 updates from Scott Kitterman
-#
-# Revision 1.4.2.3  2008/08/01 04:01:25  customdesigned
-# Release 2.3.3
-#
-# Revision 1.4.2.2  2008/08/01 03:58:03  customdesigned
-# Don't try to close socket when never opened.
-#
-# Revision 1.4.2.1  2008/07/28 19:54:13  customdesigned
-# Add pypi metadata to setup.py
-#
-# Revision 1.4  2002/05/06 06:32:07  anthonybaxter
-# filled in a blank
-#
-# Revision 1.3  2001/11/23 19:43:57  stroeder
-# Prepend current directory to sys.path to enable import of DNS.
-#
-# Revision 1.2  2001/11/23 19:36:35  stroeder
-# Use DNS.__version__ as package version and corrected name
-#
-# Revision 1.1  2001/08/09 13:42:38  anthonybaxter
-# initial setup.py. That was easy. :)
-#
-#
